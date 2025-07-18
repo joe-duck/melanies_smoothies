dfceb88d7c79c58cb633356cdb7bc99f11b397fb
joe-duck <joseph.geoghegan@siemens.com>
2025-07-17 17:25:12 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ st.write('The name of the Smoothie will be: ',name_on_order)
 my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 ingredients_list = st.multiselect('Choose up to 5 ingrediants', my_dataframe,max_selections=5)
 
 if ingredients_list:
@@ -35,6 +35,8 @@ if ingredients_list:
     st.write(ingredients_string)
     
     for fruit_chosen in ingredients_list:
+      search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+      st.write('The search value for ' , fruit_chosen, ' is ' , search_on, '.')
       smoothiefroot_response = requests.get("https://api.nal.usda.gov/fdc/v1/foods/search?api_key=b3zrMMJ1evTmkmyeJifjqg971D5McVWohHjqIx6p&query="+fruit_chosen+"%20raw&SRFoodCategory=Fruits%20and%20Fruit%20Juices")
       # st.text(smoothiefroot_response)
       food = smoothiefroot_response.json()
