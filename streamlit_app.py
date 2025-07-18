9d718e49e14505840dbf5de212ed2505ecd75e52
joe-duck <joseph.geoghegan@siemens.com>
2025-07-17 17:46:13 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if ingredients_list:
     # for fruit_chosen in ingredients_list:
     #         ingredients_string += fruit_chosen 
     # st.write(ingredients_string)
-    
+    ingredients_string = ''
     for fruit_chosen in ingredients_list:
       ingredients_string = fruit_chosen+ ' '
       search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
@@ -54,7 +54,7 @@ if ingredients_list:
       else:
           st.warning("No foods found in the API response.")
 
-      st.write(ingredients_string)
+    st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
     values('""" + ingredients_string + """','"""+name_on_order+"""')
     """
