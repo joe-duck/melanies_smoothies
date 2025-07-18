f6935ffbf5e6c9bfc9e0161e6e82de01487e3288
joe-duck <joseph.geoghegan@siemens.com>
2025-07-17 17:46:57 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ if ingredients_list:
     # st.write(ingredients_string)
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
-      ingredients_string = fruit_chosen+ ' '
+      ingredients_string += fruit_chosen+ ' '
       search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
       st.write('The search value for ' , fruit_chosen, ' is ' , search_on, '.')
       smoothiefroot_response = requests.get("https://api.nal.usda.gov/fdc/v1/foods/search?api_key=b3zrMMJ1evTmkmyeJifjqg971D5McVWohHjqIx6p&query="+search_on+"%20raw&SRFoodCategory=Fruits%20and%20Fruit%20Juices")
